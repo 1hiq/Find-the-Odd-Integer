1a4186205012d14eae944557b82ed52f36eb0ab1
HixYE <74153189+1hiq@users.noreply.github.com>
2022-06-05 15:35:36 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+int calcNumOfRepeats(int arr[], int arrLen, int num) {
+	int counter = 0;
+	for (int i = 0; i < arrLen; i++) {
+		if (arr[i] == num) {
+			counter++;
+		}
+	}
+
+	return counter;
+}
+
+int findOddRepeat(int arr[], int arrLen) {
+	for (int i = 0; i < arrLen; i++) {
+		if (calcNumOfRepeats(arr, arrLen, arr[i]) % 2 == 1) {
+			return arr[i];
+		}
+	}
+}
+
+
+int main(void) {
+  
+  // For example: this array contains some of repeated numbers, so the function (findOddRepeat) will return the element (number) which repeated odd number of times.
+	int array[] = { 20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5 };
+	int arrayLenght = sizeof(array) / sizeof(array[0]);
+	
+	cout << findOddRepeat(array, arrayLenght) << endl;
+
+	
+
+	
+	
+	return 0;
+}
+
